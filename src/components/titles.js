e87ac80c37af16f0e6d251fe3c24a18e4f4d1b03
Kirill <lambnelsen@gmail.com>
2019-01-07 15:27:54 +0200

    Init

---
+++ src/components/titles.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Titles extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+                <h1 className="title-container__title">Weather For You</h1>
+                <p className="title-container__subtitle">Help you anywhere...</p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
